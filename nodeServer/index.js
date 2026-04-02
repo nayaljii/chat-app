fdcd58e1f3ab9036dfe84f324660dda961bd3bb0
Vishal Nayal <vishalnayal325@email.com>
2026-04-02 11:13:16 +0530

    Deploy-ready code with safe routes

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -36,7 +36,7 @@ app.get('/messages', async (req, res) => {
 });
 
 // SPA catch-all (for frontend routing)
-app.get('*', (req, res) => {
+app.get(/^\/.*$/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "index.html"));
 });
 
