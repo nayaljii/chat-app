bb04f4312812a511a99a03e17c933bcf6650f1ea
Vishal Nayal <vishalnayal325@email.com>
2026-04-30 14:01:47 +0530

    emails

--- backend/utils/sendOtpEmail.js
+++ backend/utils/sendOtpEmail.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendOtpEmail(to, otp) {
     const { data, error } = await resend.emails.send({
-        from: process.env.EMAIL_FROM || "Vish'sUp <onboarding@resend.dev>",
+        from: "Vish'sUp <no-reply@vishsup.duckdns.org>",
         to: [to],
         subject: "Your OTP for Vish'sUp",
         html: `
